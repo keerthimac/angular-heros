9fae450a808ad1a8590425b6d60478395395560b
Darshana <keerthi.mac@gmail.com>
2024-05-10 07:39:25 +0530

    display list finished

--- src/app/components/heros/heros.component.ts
+++ src/app/components/heros/heros.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../hero';
-import { NgFor, UpperCasePipe } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../../mock-heros';
 
 @Component({
   selector: 'app-heros',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor],
+  imports: [UpperCasePipe, FormsModule, NgFor,NgIf],
   templateUrl: './heros.component.html',
   styleUrl: './heros.component.css',
 })
